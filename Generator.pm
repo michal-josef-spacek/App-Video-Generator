c2db7bb3a51bdc41954fc7c70500149a4dde384c
Michal Špaček <tupinek@gmail.com>
2015-09-15 22:36:13 +0200

    Prepare for 0.04 version.

--- Generator.pm
+++ Generator.pm
@@ -11,7 +11,7 @@ use Getopt::Std;
 use Video::Generator;
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -166,6 +166,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
